3ba393605b7141b31e6581294bcc2d1e0cffd322
Henry Gomez <hglerna@gmail.com>
2026-07-08 09:51:24 -0500

    chore: bump version to v1.1.10 (code 22)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bastion.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.1.9"
+        versionCode = 22
+        versionName = "1.1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
